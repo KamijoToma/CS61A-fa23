cf53942dd63b740219f23853a72ef679203b8a25
SkyRain <admin@misakacloud.net>
2024-01-27 08:54:00 +0000

    lab00

---
+++ labs/lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty_three():
+    """Come up with the most creative expression that evaluates to 2023,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_three()
+    2023
+    """
+    return -(1-145)*14+11-4+5-1-4 # Refer to https://lab.magiconch.com/homo/
+
